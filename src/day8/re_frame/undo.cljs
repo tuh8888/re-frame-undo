1b92b546d20b863e1c2d4f90126476f6e30f4707
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-04-18 07:34:10 -0600

    Undo lists are maps assoc'd to the db-atom instead.

--- src/day8/re_frame/undo.cljs
+++ src/day8/re_frame/undo.cljs
@@ -1,179 +1,219 @@
 (ns day8.re-frame.undo
-  (:require-macros [reagent.ratom  :refer [reaction]])
-  (:require
-    [reagent.core        :as     reagent]
-    [re-frame.core       :as     re-frame]
-    [re-frame.db         :refer  [app-db]]))
+  (:require-macros [reagent.ratom :refer [reaction]])
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as re-frame]
+            [re-frame.db :refer [app-db]]))
 
 
 ;; -- Configuration ----------------------------------------------------------
 
-(def config (atom {:max-undos    50   ;; Maximum number of undo states maintained
-                   :harvest-fn   deref
-                   :reinstate-fn reset!
-                   :undo-list (reagent/atom [])
-                   :redo-list (reagent/atom [])}))
+(def config
+  (atom {:max-undos         50 ;; Maximum number of undo states maintained
+         :harvest-fn        deref
+         :reinstate-fn      reset!
+         :undo-list         (reagent/atom {})
+         :redo-list         (reagent/atom {})
+         ;; -- Explanations
+         ;; -----------------------------------------------------------
+         ;;
+         ;; Each undo has an associated string explanation, for display to the
+         ;; user.
+         ;;
+         ;; It seems ugly to have mirrored vectors, but ...
+         ;; the code kinda falls out when you do. I'm feeling lazy.
+         :undo-explain-list (reagent/atom {}) ;"Mirrors undo-list"
+         :app-explain       (reagent/atom "") ;"Mirrors app-db"
+         :redo-explain-list (reagent/atom []) ;"Mirrors redo-list"
+        }))
 
 (defn undo-config!
   "Set configuration parameters for library.
 
   Should be called on app startup."
   [new-config]
-  (if-let [unknown-keys (seq (clojure.set/difference
-                               (-> new-config keys set)
-                               (-> @config keys set)))]
-    (re-frame/console :error "re-frame-undo: undo-config! called within unknown keys: " unknown-keys)
+  (if-let [unknown-keys (seq (clojure.set/difference (-> new-config
+                                                         keys
+                                                         set)
+                                                     (-> @config
+                                                         keys
+                                                         set)))]
+    (re-frame/console :error
+                      "re-frame-undo: undo-config! called within unknown keys: "
+                      unknown-keys)
     (swap! config merge new-config)))
 
 
-(defn max-undos
-  []
-  (:max-undos @config))
-
-;; -- Explanations -----------------------------------------------------------
-;;
-;; Each undo has an associated string explanation, for display to the user.
-;;
-;; It seems ugly to have mirrored vectors, but ...
-;; the code kinda falls out when you do. I'm feeling lazy.
-(def app-explain "Mirrors app-db" (reagent/atom ""))
-(def undo-explain-list "Mirrors undo-list" (reagent/atom []))
-(def redo-explain-list "Mirrors redo-list" (reagent/atom []))
+(defn max-undos [] (:max-undos @config))
 
 (defn clear-undos!
   []
-  (reset! (:undo-list @config) [])
-  (reset! undo-explain-list []))
+  (reset! (:undo-list @config) {})
+  (reset! (:undo-explain-list @config) {}))
 
 
 (defn clear-redos!
   []
-  (reset! (:redo-list @config) [])
-  (reset! redo-explain-list []))
+  (reset! (:redo-list @config) {})
+  (reset! (:redo-explain-list @config) {}))
 
 
 (defn clear-history!
   []
   (clear-undos!)
   (clear-redos!)
-  (reset! app-explain ""))
+  (reset! (:app-explain @config) ""))
 
 
 (defn store-now!
   "Stores the value currently in app-db, so the user can later undo"
   [explanation]
   (clear-redos!)
-  (reset! (:undo-list @config) (vec (take-last
-                                      (max-undos)
-                                      (conj @(:undo-list @config) ((:harvest-fn @config) app-db)))))
-  (reset! undo-explain-list (vec (take-last
-                                   (max-undos)
-                                   (conj @undo-explain-list @app-explain))))
-  (reset! app-explain explanation))
+  (swap! (:undo-explain-list @config)
+    (fn [undo-explain-list-map]
+      (update undo-explain-list-map
+              app-db
+              (fn [undo-explain-list]
+                (->> @(:app-explain @config)
+                     (conj undo-explain-list)
+                     (take-last (max-undos))
+                     vec)))))
+  (swap! (:undo-list @config)
+    (fn [undo-list-map]
+      (update undo-list-map
+              app-db
+              (fn [undo-list]
+                (->> app-db
+                     ((:harvest-fn @config))
+                     (conj undo-list)
+                     (take-last (max-undos))
+                     vec)))))
+  (reset! (:app-explain @config) explanation))
 
 
 (defn undos?
   "Returns true if undos exist, false otherwise"
   []
-  (seq @(:undo-list @config)))
+  (seq (get @(:undo-list @config) app-db)))
 
 (defn redos?
   "Returns true if redos exist, false otherwise"
   []
-  (seq @(:redo-list @config)))
+  (seq (get @(:redo-list @config) app-db)))
 
 (defn undo-explanations
   "Returns a vector of undo descriptions, perhaps empty"
   []
   (if (undos?)
-    (conj @undo-explain-list @app-explain)
+    (conj (get @(:undo-explain-list @config) app-db) @(:app-explain @config))
     []))
 
-;; -- subscriptions  -----------------------------------------------------------------------------
+;; -- subscriptions
+;; -----------------------------------------------------------------------------
 
-(re-frame/reg-sub-raw
-  :undos?                   ;;  usage:  (subscribe [:undos?])
+(re-frame/reg-sub-raw :undos?                   ;;  usage:  (subscribe
+  ;;  [:undos?])
   (fn handler
     ; "returns true if anything is stored in the undo list, otherwise false"
     [_ _]
     (reaction (undos?))))
 
-(re-frame/reg-sub-raw
-  :redos?
+(re-frame/reg-sub-raw :redos?
   (fn handler
     ; "returns true if anything is stored in the redo list, otherwise false"
     [_ _]
     (reaction (redos?))))
 
 
-(re-frame/reg-sub-raw
-  :undo-explanations
+(re-frame/reg-sub-raw :undo-explanations
   (fn handler
     ; "returns a vector of string explanations ordered oldest to most recent"
     [_ _]
     (reaction (undo-explanations))))
 
-(re-frame/reg-sub-raw
-  :redo-explanations
+(re-frame/reg-sub-raw :redo-explanations
   (fn handler
-    ; "returns a vector of string explanations ordered from most recent undo onward"
+    ; "returns a vector of string explanations ordered from most recent undo
+    ; onward"
     [_ _]
-    (reaction (deref redo-explain-list))))
+    (reaction (deref (:redo-explain-list @config)))))
 
-;; -- event handlers  ----------------------------------------------------------------------------
+;; -- event handlers
+;; ----------------------------------------------------------------------------
 
 
 (defn undo
   [harvester reinstater undos cur redos]
-  (let [u @undos
-        r (cons (harvester cur) @redos)]
+  (let [u (get @undos app-db)
+        r (cons (harvester cur) (get @redos app-db))]
     (reinstater cur (last u))
-    (reset! redos r)
-    (reset! undos (pop u))))
+    (swap! redos assoc app-db r)
+    (swap! undos assoc app-db (pop u))))
 
 
 (defn undo-n
   "undo n steps or until we run out of undos"
   [n]
   (when (and (pos? n) (undos?))
-    (undo (:harvest-fn @config) (:reinstate-fn @config) (:undo-list @config) app-db (:redo-list @config))
-    (undo deref reset! undo-explain-list app-explain redo-explain-list)
+    (undo (:harvest-fn @config)
+          (:reinstate-fn @config)
+          (:undo-list @config)
+          app-db
+          (:redo-list @config))
+    (undo deref
+          reset!
+          (:undo-explain-list @config)
+          (:app-explain @config)
+          (:redo-explain-list @config))
     (recur (dec n))))
 
 (defn undo-handler
   [_ [_ n]]
   (if-not (undos?)
-    (re-frame/console :warn "re-frame: you did a (dispatch [:undo]), but there is nothing to undo.")
+    (re-frame/console
+     :warn
+     "re-frame: you did a (dispatch [:undo]), but there is nothing to undo.")
     (undo-n (or n 1)))
-  {}) ; work is done directly on app-db
+  {}); work is done directly on app-db
 
 (defn redo
   [harvester reinstater undos cur redos]
-  (let [u (conj @undos (harvester cur))
-        r  @redos]
+  (let [u (conj (get @undos app-db) (harvester cur))
+        r (get @redos app-db)]
     (reinstater cur (first r))
-    (reset! redos (rest r))
-    (reset! undos u)))
+    (swap! redos assoc app-db (rest r))
+    (swap! undos assoc app-db u)))
 
 (defn redo-n
   "redo n steps or until we run out of redos"
   [n]
   (when (and (pos? n) (redos?))
-    (redo (:harvest-fn @config) (:reinstate-fn @config) (:undo-list @config) app-db (:redo-list @config))
-    (redo deref reset! undo-explain-list app-explain redo-explain-list)
+    (redo (:harvest-fn @config)
+          (:reinstate-fn @config)
+          (:undo-list @config)
+          app-db
+          (:redo-list @config))
+    (redo deref
+          reset!
+          (:undo-explain-list @config)
+          (:app-explain @config)
+          (:redo-explain-list @config))
     (recur (dec n))))
 
 (defn redo-handler
   [_ [_ n]]  ;; if n absent, defaults to 1
-    (if-not (redos?)
-      (re-frame/console :warn "re-frame: you did a (dispatch [:redo]), but there is nothing to redo.")
-      (redo-n (or n 1)))
-    {})      ; work is done directly on app-db
+  (if-not (redos?)
+    (re-frame/console
+     :warn
+     "re-frame: you did a (dispatch [:redo]), but there is nothing to redo.")
+    (redo-n (or n 1)))
+  {}); work is done directly on app-db
 
 (defn purge-redo-handler
   [db _]
   (if-not (redos?)
-    (re-frame/console :warn "re-frame: you did a (dispatch [:purge-redos]), but there is nothing to redo.")
+    (re-frame/console
+     :warn
+     "re-frame: you did a (dispatch [:purge-redos]), but there is nothing to redo.")
     (clear-redos!))
   db)
 
@@ -192,21 +232,25 @@
   "
   ([] (undoable nil))
   ([explanation]
-      (re-frame/->interceptor
-        :id     :undoable
-        :after  (fn [context]
-                  (let [event        (re-frame/get-coeffect context :event)
-                        undo-effect  (re-frame/get-effect context :undo)
-                        explanation (cond
-                                      (some? undo-effect)   undo-effect
-                                      (fn? explanation)     (explanation
-                                                              (re-frame/get-coeffect context :db)
-                                                              event)
-                                      (string? explanation) explanation
-                                      (nil? explanation)    ""
-                                      :else (re-frame/console :error "re-frame-undo: \"undoable\" interceptor on event " event " given a bad parameter. Got: " explanation))]
-                    (store-now! explanation)
-                    (update context :effects dissoc :undo))))))   ;; remove any `:undo` effect. Already handled.
+   (re-frame/->interceptor
+    :id :undoable
+    :after
+    (fn [context]
+      (let [event (re-frame/get-coeffect context :event)
+            undo-effect (re-frame/get-effect context :undo)
+            explanation
+            (cond (some? undo-effect) undo-effect
+                  (fn? explanation)
+                  (explanation (re-frame/get-coeffect context :db) event)
+                  (string? explanation) explanation
+                  (nil? explanation) ""
+                  :else (re-frame/console
+                         :error
+                         "re-frame-undo: \"undoable\" interceptor on event "
+                         event
+                         " given a bad parameter. Got: " explanation))]
+        (store-now! explanation)
+        (update context :effects dissoc :undo))))));; remove any `:undo` effect. Already handled.
 
 
 ;; -- register handlers for events and subscriptions
@@ -214,14 +258,14 @@
 
 (defn register-events-subs!
   []
-  (re-frame/reg-event-fx
-    :undo                     ;; usage:  (dispatch [:undo n])  n is optional, defaults to 1
+  (re-frame/reg-event-fx :undo
+    ;; usage:  (dispatch [:undo n])  n is optional, defaults to 1
     undo-handler)
-  (re-frame/reg-event-fx
-    :redo                     ;; usage:  (dispatch [:redo n])
+  (re-frame/reg-event-fx :redo
+    ;; usage:  (dispatch [:redo n])
     redo-handler)
-  (re-frame/reg-event-db
-    :purge-redos              ;; usage:  (dispatch [:purge-redos])
+  (re-frame/reg-event-db :purge-redos
+    ;; usage:  (dispatch [:purge-redos])
     purge-redo-handler))
 
 (register-events-subs!)
--- test/day8/re_frame/undo/undo_test.cljs
+++ test/day8/re_frame/undo/undo_test.cljs
@@ -1,34 +1,31 @@
 (ns day8.re-frame.undo.undo-test
-  (:require [cljs.test          :refer-macros [is deftest]]
+  (:require [cljs.test :refer-macros [is deftest]]
             [day8.re-frame.undo :as undo]
-            [re-frame.db        :as db]
-            [re-frame.core      :as re-frame]))
+            [re-frame.db :as db]
+            [re-frame.core :as re-frame]))
 
 (defn undo-fixtures
   [f]
   (let [restore-re-frame-fn (re-frame/make-restore-fn)]
     (undo/undo-config! {:max-undos 5})
     (undo/clear-history!)
-    (try
-      (f)
-      (finally (restore-re-frame-fn)))))
+    (try (f) (finally (restore-re-frame-fn)))))
 
 (cljs.test/use-fixtures :each undo-fixtures)
 
 (deftest test-undos
   (is (not (undo/undos?)))
   (is (not (undo/redos?)))
-
   (doseq [i (range 10)]
     (undo/store-now! (inc i))
     (reset! db/app-db {:test (inc i)}))
-
-  ;; Check the undo state is correct
+  ;; Check the undo state is
+  ;; correct
   (is (undo/undos?))
   (is (not (undo/redos?)))
   (is (= [5 6 7 8 9 10] (undo/undo-explanations)))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}] @(:undo-list @undo/config)))
-
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config)))
   ;; Undo the actions
   (re-frame/dispatch-sync [:undo])
   (is (= @db/app-db {:test 9}))
@@ -43,15 +40,13 @@
   (is (= @db/app-db {:test 5}))
   (is (not (undo/undos?)))
   (is (undo/redos?))
-
   ;; Redo them again
   (re-frame/dispatch-sync [:redo 5])
   (is (= @db/app-db {:test 10}))
   (is (not (undo/redos?)))
   (is (undo/undos?))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
-        @(:undo-list @undo/config)))
-
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config)))
   ;; Clear history
   (undo/clear-history!)
   (is (not (undo/undos?)))
@@ -59,24 +54,18 @@
 
 
 (deftest test-undos-interceptor
-
-  (re-frame/reg-event-db
-    :change-db
+  (re-frame/reg-event-db :change-db
     (undo/undoable "change-db")
-    (fn [db _]
-      (update db :test inc)))
-
-  (doseq [i (range 10)]
-    (re-frame/dispatch-sync [:change-db]))
-
+    (fn [db _] (update db :test inc)))
+  (doseq [i (range 10)] (re-frame/dispatch-sync [:change-db]))
   ;; Check the undo state is correct
   (is (undo/undos?))
   (is (not (undo/redos?)))
-  (is (= ["change-db" "change-db" "change-db" "change-db" "change-db" "change-db"]
+  (is (= ["change-db" "change-db" "change-db" "change-db" "change-db"
+          "change-db"]
          (undo/undo-explanations)))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
-        @(:undo-list @undo/config)))
-
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config)))
   ;; Undo the actions
   (re-frame/dispatch-sync [:undo])
   (is (= @db/app-db {:test 9}))
@@ -91,37 +80,30 @@
   (is (= @db/app-db {:test 5}))
   (is (not (undo/undos?)))
   (is (undo/redos?))
-
   ;; Redo them again
   (re-frame/dispatch-sync [:redo 5])
   (is (= @db/app-db {:test 10}))
   (is (not (undo/redos?)))
   (is (undo/undos?))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
-        @(:undo-list @undo/config))))
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config))))
 
 (deftest test-undos-interceptor-description
-
-  (re-frame/reg-event-fx
-    :change-db-fx
+  (re-frame/reg-event-fx :change-db-fx
     (undo/undoable)
     (fn [{:keys [db]} _]
       (let [update-num (inc (:test db))]
-        {:db (assoc db :test update-num)
+        {:db   (assoc db :test update-num)
          :undo (str "change-db " update-num)})))
-
-  (doseq [i (range 10)]
-    (re-frame/dispatch-sync [:change-db-fx]))
-
+  (doseq [i (range 10)] (re-frame/dispatch-sync [:change-db-fx]))
   ;; Check the undo state is correct
   (is (undo/undos?))
   (is (not (undo/redos?)))
   (is (= ["change-db 5" "change-db 6" "change-db 7" "change-db 8" "change-db 9"
           "change-db 10"]
          (undo/undo-explanations)))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
-        @(:undo-list @undo/config)))
-
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config)))
   ;; Undo the actions
   (re-frame/dispatch-sync [:undo])
   (is (= @db/app-db {:test 9}))
@@ -136,11 +118,10 @@
   (is (= @db/app-db {:test 5}))
   (is (not (undo/undos?)))
   (is (undo/redos?))
-
   ;; Redo them again
   (re-frame/dispatch-sync [:redo 5])
   (is (= @db/app-db {:test 10}))
   (is (not (undo/redos?)))
   (is (undo/undos?))
-  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
-        @(:undo-list @undo/config))))
+  (is (= {db/app-db [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]}
+         @(:undo-list @undo/config))))
